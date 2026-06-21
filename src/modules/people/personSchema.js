5759a80b443f73558250335d978c9c2b5c532aa1
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-06-22 00:29:59 +0800

    feat: add person data schema

---
+++ src/modules/people/personSchema.js
@@ -0,0 +1,63 @@
+import { createSkillSet } from '../../data/constants/skills.js';
+
+export const PEOPLE_SCHEMA_VERSION = 1;
+
+export function createBlankPerson() {
+  return {
+    schemaVersion: PEOPLE_SCHEMA_VERSION,
+    id: '',
+    revision: 1,
+    identity: {
+      name: '',
+      gender: 'unspecified',
+      portraitSeed: '',
+      birth: { year: 1, day: 1 },
+      alive: true,
+      death: null,
+    },
+    location: {
+      regionId: 'starting-valley',
+      tileX: null,
+      tileY: null,
+      homeId: null,
+    },
+    work: {
+      occupation: 'unassigned',
+      skills: createSkillSet(),
+      preferences: [],
+    },
+    state: {
+      hunger: 20,
+      thirst: 20,
+      energy: 100,
+      health: 100,
+      mood: 0,
+      stress: 0,
+      bodyTemperature: 36.8,
+      injuries: [],
+      statusTags: [],
+    },
+    traits: [],
+    family: {
+      parentIds: [],
+      childIds: [],
+      spouseId: null,
+      siblingIds: [],
+    },
+    relations: {},
+    memories: {
+      lifeEvents: [],
+      recent: [],
+      player: [],
+    },
+    inventory: {
+      items: {},
+      equipment: {},
+      ownedResources: {},
+      claims: [],
+    },
+    extensions: {},
+    createdAt: null,
+    updatedAt: null,
+  };
+}
