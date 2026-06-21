4d936bf914df418ae014ab447d4d9f717f6cc63a
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-06-22 02:54:02 +0800

    feat: start farm runtime with world

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import './app-v4.js';
 import { attachEcologyRuntime } from './bootstrap/attachEcologyRuntime.js';
 import { attachRoadRuntime } from './bootstrap/attachRoadRuntime.js';
+import { attachFarmRuntime } from './bootstrap/attachFarmRuntime.js';
 
 attachEcologyRuntime();
 attachRoadRuntime();
+attachFarmRuntime();
