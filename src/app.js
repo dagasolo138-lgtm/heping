de7dce7b41e58fc7d9207568e1c20b7e5f0bb3d7
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-06-22 03:14:56 +0800

    feat(seasons): start season runtime before farming

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import './app-v4.js';
 import { attachEcologyRuntime } from './bootstrap/attachEcologyRuntime.js';
 import { attachRoadRuntime } from './bootstrap/attachRoadRuntime.js';
+import { attachSeasonRuntime } from './bootstrap/attachSeasonRuntime.js';
 import { attachFarmRuntime } from './bootstrap/attachFarmRuntime.js';
 import { attachFoodStorageRuntime } from './bootstrap/attachFoodStorageRuntime.js';
 
 attachEcologyRuntime();
 attachRoadRuntime();
+attachSeasonRuntime();
 attachFarmRuntime();
 attachFoodStorageRuntime();
