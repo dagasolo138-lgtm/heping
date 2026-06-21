27e9fb1ae4f54e7f549376101f4d589f6dac79ba
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-06-22 02:40:20 +0800

    feat: start road runtime with world

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import './app-v4.js';
 import { attachEcologyRuntime } from './bootstrap/attachEcologyRuntime.js';
+import { attachRoadRuntime } from './bootstrap/attachRoadRuntime.js';
 
 attachEcologyRuntime();
+attachRoadRuntime();
