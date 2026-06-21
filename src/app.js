89c196a05aee82c11bcf343e2f7f6ef17b4f3f48
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-06-22 03:30:07 +0800

    feat(farming): start expansion runtime after farms

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import { attachEcologyRuntime } from './bootstrap/attachEcologyRuntime.js';
 import { attachRoadRuntime } from './bootstrap/attachRoadRuntime.js';
 import { attachSeasonRuntime } from './bootstrap/attachSeasonRuntime.js';
 import { attachFarmRuntime } from './bootstrap/attachFarmRuntime.js';
+import { attachFarmExpansionRuntime } from './bootstrap/attachFarmExpansionRuntime.js';
 import { attachFoodStorageRuntime } from './bootstrap/attachFoodStorageRuntime.js';
 
 attachEcologyRuntime();
 attachRoadRuntime();
 attachSeasonRuntime();
 attachFarmRuntime();
+attachFarmExpansionRuntime();
 attachFoodStorageRuntime();
