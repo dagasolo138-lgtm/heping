695d3685fe26a92966397d64001f9365952c7982
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-06-22 02:48:25 +0800

    feat: add batched terrain updates for farmland

--- src/modules/map/mapSystem.js
+++ src/modules/map/mapSystem.js
@@ -42,6 +42,11 @@ export function createMapSystem({ eventBus, gameTime }) {
     eventBus.emit('map:hydrated', { map: get() });
     return get();
   }
+  function setTerrainBatch(tiles, terrainId, reason = 'terrain:batch') {
+    const current = requireMap();
+    tiles.forEach(({ x, y }) => setTerrainAt(current, x, y, terrainId));
+    commit(reason);
+  }
 
   return Object.freeze({
     createStartingValley,
@@ -57,6 +62,7 @@ export function createMapSystem({ eventBus, gameTime }) {
     findNearestWalkableNeighbor: (targetX, targetY, fromX, fromY) => map ? findNearestWalkableNeighbor(map, targetX, targetY, fromX, fromY) : null,
     findNearestWaterAccess: (fromX, fromY) => map ? findNearestWaterAccess(map, fromX, fromY) : null,
     setTerrain: (x, y, terrainId) => { setTerrainAt(requireMap(), x, y, terrainId); commit('terrain:set'); },
+    setTerrainBatch,
     addFeature: (feature) => { addFeature(requireMap(), feature); commit('feature:add'); return clone(feature); },
     removeFeature: (featureId) => {
       const removed = removeFeature(requireMap(), featureId);
