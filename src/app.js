2cb19ec5450aa30086e2e3ff16be96e865d4ffaf
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-06-22 03:06:04 +0800

    feat: start food storage runtime with world

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import './app-v4.js';
 import { attachEcologyRuntime } from './bootstrap/attachEcologyRuntime.js';
 import { attachRoadRuntime } from './bootstrap/attachRoadRuntime.js';
 import { attachFarmRuntime } from './bootstrap/attachFarmRuntime.js';
+import { attachFoodStorageRuntime } from './bootstrap/attachFoodStorageRuntime.js';
 
 attachEcologyRuntime();
 attachRoadRuntime();
 attachFarmRuntime();
+attachFoodStorageRuntime();
