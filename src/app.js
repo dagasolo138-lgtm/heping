7945527d0f5ce654e07bc7a0ea90660eb2bb6ae5
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-06-22 10:17:23 +0800

    feat(time): start world speed controls

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import { attachSeasonRuntime } from './bootstrap/attachSeasonRuntime.js';
 import { attachFarmRuntime } from './bootstrap/attachFarmRuntime.js';
 import { attachFarmExpansionRuntime } from './bootstrap/attachFarmExpansionRuntime.js';
 import { attachFoodStorageRuntime } from './bootstrap/attachFoodStorageRuntime.js';
+import { attachWorldSpeedRuntime } from './bootstrap/attachWorldSpeedRuntime.js';
 import { attachMapHudRuntime } from './bootstrap/attachMapHudRuntime.js';
 import { attachBuildInfoRuntime } from './bootstrap/attachBuildInfoRuntime.js';
 
@@ -14,5 +15,6 @@ attachSeasonRuntime();
 attachFarmRuntime();
 attachFarmExpansionRuntime();
 attachFoodStorageRuntime();
+attachWorldSpeedRuntime();
 attachMapHudRuntime();
 attachBuildInfoRuntime();
