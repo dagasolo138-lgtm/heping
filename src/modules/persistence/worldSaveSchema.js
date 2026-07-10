d304a9fb6d02b073cf66a5878bc13d6ac0c8d2ff
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-07-10 23:15:02 +0800

    Bump world save app version to 0.26.0

--- src/modules/persistence/worldSaveSchema.js
+++ src/modules/persistence/worldSaveSchema.js
@@ -1,5 +1,5 @@
 export const WORLD_SAVE_SCHEMA_VERSION = 1;
-export const WORLD_SAVE_APP_VERSION = '0.25.3.1';
+export const WORLD_SAVE_APP_VERSION = '0.26.0';
 export const WORLD_SAVE_DEFAULT_SLOT = 'manual';
 export const WORLD_SAVE_STORAGE_PREFIX = 'shengling.save';
 
